01c8ab1edd4a58ec4e2c49dba1127311426d56c5
geethikareddyk9 <85934185+geethikareddyk9@users.noreply.github.com>
2023-03-15 15:51:33 +0530

    Add files via upload

---
+++ sum20.rb
@@ -0,0 +1,9 @@
+def sum20(x,y)
+    return x == 20 || y == 20 || x + y
+end
+
+print sum20(10, 10),"\n"
+print sum20(40, 10),"\n"
+print sum20(15, 20),"\n"
+print sum20(20, 20),"\n"
+print sum20(20, 10),"\n"
